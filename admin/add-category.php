9193f8c0c38c3009407e2d7c103fcc9d6eff3014
huynth1801 <huuhuy1801@gmail.com>
2023-11-26 19:50:19 +0700

    add category

--- admin/add-category.php
+++ admin/add-category.php
@@ -2,162 +2,84 @@
     include('partials/menu.php');
 ?>
 
-<div class="main-content">
-    <div class="wrapper">
-        <h1>Add Category</h1>
+<?php
+    if (isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $image_url = $_POST['image_url'];
+        $featured = $_POST['featured'];
+        $active = $_POST['active'];
 
-        <?php 
-            if(isset($_SESSION['add']))
-            {
-                echo $_SESSION['add'];
-                unset($_SESSION['add']);
-            }
+        // Tạo câu truy vấn để chèn dữ liệu vào cơ sở dữ liệu
+        $sql = "INSERT INTO tbl_category (title, image_name, featured, active) VALUES ('$title', '$image_url', '$featured', '$active')";
+        $res = mysqli_query($conn, $sql);
 
-            if(isset($_SESSION['upload']))
-            {
-                echo $_SESSION['upload'];
-                unset($_SESSION['upload']);
-            }
-        ?>
+        // 4. Cheeck whether the query executed or not data added or not
+        if($res == true)
+        {
+            // Query Executed and Category Added
+            $_SESSION['add'] = "<div class='success'>Category Added Successfully.</div>";
+            // Redirect to Manage Category Page
+            header('location:'.SITEURL.'admin/manage-category.php');
+        }
+        else
+        {
+            //. Failed to Add category
+            $_SESSION['add'] = "<div class='error'>Failed to Add Category</div>";
+            // Redirect to Manage Category Page
+            header('location:'.SITEURL.'admin/add-category.php');
+        }
+    }
 
-        <br><br>
+    mysqli_close($conn);
+?>
 
-        <!-- Add Category From Start -->
-        <form action="" method="post" enctype="multipart/form-data">
-            <table class="tbl-30">
-                <tr>
-                    <td>Title: </td>
-                    <td>
-                        <input type="text" name="title" placeholder="Category Title">
-                    </td>
-                </tr>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Category</title>
+</head>
+<body>
+    <h1>Add Category</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <table class="tbl-30">
+            <tr>
+                <td>Title: </td>
+                <td>
+                    <input type="text" name="title" placeholder="Category Title" required>
+                </td>
+            </tr>
 
-                <tr>
-                    <td>Select Image:</td>
-                    <td>
-                        <!-- <label for="url">Image URL:</label> -->
-                        <input type="file" name="image" required>
-                    </td>
-                </tr>
+            <tr>
+                <td>Select Image:</td>
+                <td>
+                    <input type="text" name="image_url" placeholder="Image URL" required>
+                </td>
+            </tr>
 
-                <tr>
-                    <td>Featured: </td>
-                    <td>
-                        <input type="radio" name="featured" value="Yes"> Yes
-                        <input type="radio" name="featured" value="No"> No
-                    </td>
-                </tr>
+            <tr>
+                <td>Featured: </td>
+                <td>
+                    <input type="radio" name="featured" value="Yes"> Yes
+                    <input type="radio" name="featured" value="No"> No
+                </td>
+            </tr>
 
-                <tr>
-                    <td>Active: </td>
-                    <td>
-                        <input type="radio" name="active" value="Yes">Yes
-                        <input type="radio" name="active" value="No">No
-                    </td>
-                </tr>
+            <tr>
+                <td>Active: </td>
+                <td>
+                    <input type="radio" name="active" value="Yes">Yes
+                    <input type="radio" name="active" value="No">No
+                </td>
+            </tr>
 
-                <tr>
-                    <td colspan="2">
-                        <input type="submit" name="submit" value="Add category" class="btn btn-primary">
-                    </td>
-                </tr>
-            </table>
-        </form>
-        <!-- Add Category Form End -->
-        <?php 
-            // Check whether the Submit button is Clicked or Not
-            if(isset($_POST['submit']))
-            {
-                // echo "Clicked";
-                // 1. Get the Value from Category Form
-                $title = $_POST['title'];
-
-                // For Radio Input, we need to check whether the button is selected or not
-                if(isset($_POST['featured']))
-                {
-                    // Get the value from form
-                    $featured = $_POST['featured'];
-                }
-                else
-                {
-                    // Set the default value
-                    $featured = "No";
-                }
-
-                if(isset($_POST['active']))
-                {
-                    $active = $_POST['active'];
-                }
-                else
-                {
-                    $active = "No";
-                }
-                // Check whether the image is selected or not and set the value for image 
-                // print_r($_FILES['image']);
-
-                // die(); // Break the code here
-                if(isset($_FILES['image']['name']))
-                {
-                    // Upload the Image
-                    // To upload image we need image name, source path and destination path
-                    $image_name = $_FILES['image']['name'];
-
-                    // Auto rename our image
-                    // Get the extension of our image 
-                    $ext = explode('.', $image_name);
-
-                    $source_path = $_FILES['image']['tmp_name'];
-                    $destination_path = "../images/category/".$image_name;
-
-                    // Finally Upload the Image
-                    $upload = move_uploaded_file($source_path, $destination_path);
-
-                    // Check whether the image is uploaded or not
-                    // And if the image is not uploaded then we will stop the process and redirect withe error message
-                    if(!$upload)
-                    {                
-                        $_SESSION['upload'] = "<div class='error'>Failed to Upload Image. </div>";
-                        // Redirect to Add Category
-                        header('location:'.SITEURL.'admin/add-category.php');
-                        // Stop the process
-                        exit();
-                    }
-                }
-                else
-                {
-                    // Don't Upload Image and set the image_name value as blank
-                    $image_name = "";
-                }
-
-
-                // 2. Create SQL Query to Insert Category into Database
-                $sql = "INSERT INTO tbl_category SET
-                        title='$title',
-                        featured='$featured',
-                        active='$active'
-                ";
-
-                // 3. Execute the Query and Save in Database
-                $res = mysqli_query($conn, $sql);
-
-                // 4. Cheeck whether the query executed or not data added or not
-                if($res == true)
-                {
-                    // Query Executed and Category Added
-                    $_SESSION['add'] = "<div class='success'>Category Added Successfully.</div>";
-                    // Redirect to Manage Category Page
-                    header('location:'.SITEURL.'admin/manage-category.php');
-                }
-                else
-                {
-                    //. Failed to Add category
-                    $_SESSION['add'] = "<div class='error'>Failed to Add Category</div>";
-                    // Redirect to Manage Category Page
-                    header('location:'.SITEURL.'admin/add-category.php');
-                }
-            }
-        ?>
-    </div>
-</div>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="submit" value="Add category" class="btn btn-primary">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
 
 <?php include('partials/footer.php');?>
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -21,42 +21,70 @@
         <br /><br /><br />
         
         <table class="tbl-full">
-            <tr>
-                <th>S.N.</th>
-                <th>Full Name</th>
-                <th>Username</th>
-                <th>Actions</th>
-            </tr>
+            
 
-            <tr>
-                <td>1.</td>
-                <td>Team 4</td>
-                <td>team4</td>
-                <td>
-                    <a href="#" class="btn btn-secondary">Update Admin</a>
-                    <a href="#" class="btn btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            <?php 
+                // Query tp get all Categories from Database
+                $sql = "SELECT * FROM tbl_category";
 
-            <tr>
-                <td>2.</td>
-                <td>Team 4</td>
-                <td>team4</td>
-                <td>
-                    <a href="#" class="btn btn-secondary ">Update Admin</a>
-                    <a href="#" class="btn btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                // Execute Query
+                $res = mysqli_query($conn, $sql);
 
-            <tr>
-                <td>3.</td>
-                <td>Team 4</td>
-                <td>team4</td>
-                <td>
-                    <a href="#" class="btn btn-secondary ">Update Admin</a>
-                    <a href="#" class="btn btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                // Count rows
+                $rows = mysqli_num_rows($res);
+
+                // Create Serial Number variable and assgin value 1
+                $sn = 1;
+
+                // Check whether we have data in database or not
+                if($rows>0){
+                   // Get the data and display
+                   while($rows=mysqli_fetch_assoc($res))
+                   {
+                        $id = $row['id'];
+                        $title = $rows['title'];
+                        $image_name = $rows['image_name'];
+                        $featured = $rows['featured'];
+                        $active = $rows['active'];
+
+                        ?>
+                            <tr>
+                                <th>S.N.</th>
+                                <th>Title</th>
+                                <th>Image</th>
+                                <th>Featured</th>
+                                <th>Active</th>
+                                <th>Actions</th>
+                            </tr>
+
+                            <tr>
+                                <td><?php echo $sn++; ?></td>
+                                <td><?php echo $title; ?></td>
+                                <td>
+                                    <img src="<?php echo $image_name; ?>" alt="" width="100px">
+                                </td>
+                                <td><?php echo $featured; ?></td>
+                                <td><?php echo $active; ?></td>
+                                <td class="">
+                                    <a href="#" class="btn btn-secondary">Update Category</a>
+                                    <a href="#" class="btn btn-danger">Delete Category</a>
+                                </td>
+                            </tr>
+                        <?php 
+                        
+                   }
+                }
+                else {
+                    // We do not have data
+                    // We will display the message inside table
+                    ?>
+                        <tr>
+                            <td colspan="6"><div class="error">No Category Added.</div></td>
+                        </tr>
+                    <?php
+                }
+            ?>
+
         </table>
     </div>
 </div>
